3bbc431332e018a7ff58481067cd9e6f2578b7a0
Suncerie Daye <sunceried@gmail.com>
2021-07-27 16:44:06 -0700

    Readme edited

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import SortingVisualizer from './SortingVisualizer/SortingVisualizer.js';
+
+function App() {
+  return (
+    <div className="App">
+      <SortingVisualizer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/SortingVisualizer/SortingVisualizer.js
@@ -0,0 +1,68 @@
+import React, { useEffect, useState } from 'react';
+import './SortingVisualizer.css';
+
+export default function SortingVisualizer(){
+    const [array, setArray] =  useState([]);
+    const [arraySize,setArraySize] = useState(100); //The user will eventually be able change the array size
+
+    /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~HELPERS~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+    /**********resetArray**********
+        @description
+            gives the visualized array new values
+        @params
+            none
+        @returns
+            none
+    */
+   function resetArray(){
+       const newArray = [];
+       //populate the new away array
+       for(let i = 0; i < arraySize; i++){
+           newArray.push(randomIntFromInterval(10,500));
+       }
+       setArray(newArray);
+   }
+    /**********randomIntFromInterval**********
+    @description
+
+    @params
+        min - integer value that represents the minimum value
+        max - integer value that represents the maximum value
+    @return 
+        random integer in between min and max
+    */ 
+    function randomIntFromInterval(min, max){
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    }
+
+    /* */
+    function createArrayValueBars(){
+        return array.map((value, index) => (
+            <div className="array-bar"
+                key={index}
+                style={{height: `${value}px`}}>
+            </div>
+        ));
+    }
+
+    useEffect(()=>{
+        resetArray();
+    },[]); // Replace compnenetDidMount. We want the array to reset when the page 
+
+
+    //Render
+    return (
+        <div className="array-container">
+            {createArrayValueBars()}
+        </div>
+        
+    );
+
+ 
+     
+
+    
+
+    
+}
